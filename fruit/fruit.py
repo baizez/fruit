c13c56d345bb072b0ebc66428888ee1d957620e3
909041282 <37203086+909041282@users.noreply.github.com>
2018-04-04 17:48:01 +0800

    svm 训练，mat2csv更改

--- fruit/fruit.py
+++ fruit/fruit.py
@@ -1,8 +1,18 @@
 from sklearn import svm
-import csv
+import scipy.io as sio  
+import matplotlib.pyplot as plt  
+import numpy as np
 
+#np.set_printoptions(threshold=np.NaN) 
+data = sio.loadmat('data/fruit0326.mat')
+Xtrain=data['CSH_train_baseline']
+Xtest=data['CSH_test_baseline']
+YtrainLabel=data['TH_train']
+Ytrain=list()
+for row in YtrainLabel:
+    Ytrain.append(row[0]*1+row[1]*2+row[2]*3+row[3]*4)
 
-with open('csv/fruit0326_FSH_train_1_0_.csv') as traindata:
-    train = csv.reader(traindata)
-    for row in train:
-        print(row)
+clf=svm.SVC(kernel='linear',decision_function_shape='ovo')
+res=clf.fit(Xtrain,Ytrain)
+print(res)
+print(clf.predict(Xtest))
